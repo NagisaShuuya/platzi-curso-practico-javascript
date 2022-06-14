af8434eb704ee75d51809e58548328e850f406d5
Nagisachii <mokakyuu@gmail.com>
2022-06-13 20:12:35 -0500

    Añadí el index.html al resto de documentos

---
+++ js/salarios.js
@@ -0,0 +1,81 @@
+const colombia = [];
+colombia.push ({
+    name: "Juan",
+    salary: 800,
+});
+colombia.push ({
+    name: "Rosa",
+    salary: 8000,
+});
+colombia.push ({
+    name: "Ernesto",
+    salary: 300,
+});
+colombia.push ({
+    name: "Lilia",
+    salary: 1000,
+});
+colombia.push ({
+    name: "Oscar",
+    salary: 40000,
+});
+colombia.push ({
+    name: "Emilia",
+    salary: 5000,
+});
+colombia.push ({
+    name: "Carlos",
+    salary: 600,
+});
+colombia.push ({
+    name: "Patricia",
+    salary: 7000,
+});
+colombia.push ({
+    name: "Axel",
+    salary: 4500,
+});
+colombia.push ({
+    name: "Kairi",
+    salary: 6400,
+});
+colombia.push ({
+    name: "Sora",
+    salary: 9100,
+});
+colombia.push ({
+    name: "Namine",
+    salary: 5700,
+});
+colombia.push ({
+    name: "Roxas",
+    salary: 4065,
+});
+colombia.push ({
+    name: "Ariel",
+    salary: 7452,
+});
+colombia.push ({
+    name: "Ventus",
+    salary: 2415,
+});
+colombia.push ({
+    name: "Aqua",
+    salary: 1424,
+});
+colombia.push ({
+    name: "Locke",
+    salary: 2875,
+});
+colombia.push ({
+    name: "Terra",
+    salary: 3684,
+});
+colombia.push ({
+    name: "Cloud",
+    salary: 5725,
+});
+colombia.push ({
+    name: "Zack",
+    salary: 25400,
+});
